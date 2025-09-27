d814406503631d62e91ce8bb0f923a9270660516
anand <anandvaibhaw000@gmail.com>
2025-09-27 22:39:24 +0530

    updated app with react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Dashboard from './components/Dashboard'
+import DashboardAlpha from './components/DashboardAlpha'
+
 function App() {
   return (
-    <div>
-      <Dashboard />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/alpha" element={<DashboardAlpha />} />
+      </Routes>
+    </Router>
   )
 }
+
 export default App
